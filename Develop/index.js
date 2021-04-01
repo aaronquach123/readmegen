02e68192344f7bad244b600871166baa11726b1e
aaron <aaronquach12341@gmail.com>
2021-04-01 08:48:23 -0700

    started user prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,46 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questionsPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the project title?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please provide a project title!")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please enter project description"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please describe the installation process.(Optional)"
+        },
+        {
+
+        }
+
+    ]) 
+}
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("test")
+}
 
 // Function call to initialize app
 init();
